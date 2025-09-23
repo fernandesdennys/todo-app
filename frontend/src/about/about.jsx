e24049ac0a3ae99353a42bfb7fea64b84e3b0bac
Dennys Fernandes <dennys.projetos@outlook.com>
2025-09-23 13:46:05 -0300

    Add routing and page headers to app
    Introduced a new routes component using react-router to handle navigation between Todo and About pages. Added a reusable PageHeader component and updated both About and Todo pages to use it for consistent headers. Refactored app.jsx to use the new Routes component instead of rendering Todo and About directly.

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+import PageHeader from '../template/pageHeader';
 
-export default props => (
+export default (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+    <h1> Nossa História</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      adipisci, ab ipsum in, laboriosam totam ipsa eos amet numquam eius ad
+      perferendis! Quidem impedit quod in explicabo animi hic eligendi!
+    </p>
+    <h1> Missão e Visão</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      adipisci, ab ipsum in, laboriosam totam ipsa eos amet numquam eius ad
+      perferendis! Quidem impedit quod in explicabo animi hic eligendi!
+    </p>
+
+    <h1> Imprensa </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      adipisci, ab ipsum in, laboriosam totam ipsa eos amet numquam eius ad
+      perferendis! Quidem impedit quod in explicabo animi hic eligendi!
+    </p>
   </div>
-)
+);
