c7a181495e2f64a6ad80ae15fc9aaa4bf4cee9c0
Dennys Fernandes <dennys.projetos@outlook.com>
2025-11-18 16:43:30 -0300

    Integrate todo search with Redux and refactor TodoForm
    Added a search action using axios to fetch todos from the API and update the Redux store. Refactored TodoForm from a functional to a class component to better handle key events and lifecycle methods, and improved prop handling for parent-controlled state. Enabled Redux DevTools in the store for easier debugging.

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { changeDescription } from './todoActions';
+import { changeDescription, search } from './todoActions';
 
-const TodoForm = (props) => {
-  const keyHandler = (e) => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.keyHandler = this.keyHandler.bind(this);
+  }
+
+  componentWillMount() {
+    // chama a busca do pai (mantemos a conexão com Redux, mas o pai controla o estado)
+    if (this.props.handleSearch) this.props.handleSearch();
+  }
+
+  keyHandler(e) {
     if (e.key === 'Enter') {
-      e.shiftKey ? props.handleSearch() : props.handleAdd();
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
     } else if (e.key === 'Escape') {
-      props.handleClear();
+      // usar this.props (corrige referência indefinida)
+      if (this.props.handleClear) this.props.handleClear();
     }
-  };
+  }
 
-  return (
-    <div role="form" className="todoForm">
-      <Grid cols="12 9 10">
-        <input
-          id="description"
-          className="form-control"
-          placeholder="Adicione uma tarefa"
-          onChange={props.changeDescription}
-          onKeyUp={keyHandler}
-          value={props.description}
-        />
-      </Grid>
+  render() {
+    return (
+      <div role="form" className="todoForm">
+        <Grid cols="12 9 10">
+          <input
+            id="description"
+            className="form-control"
+            placeholder="Adicione uma tarefa"
+            // o pai (componente Todo) controla o estado local; usar handleChange passado pelo pai
+            onChange={this.props.handleChange}
+            onKeyUp={this.keyHandler}
+            value={this.props.description}
+          />
+        </Grid>
 
-      <Grid cols="12 3 2">
-        <IconButton style="primary" icon="plus" onClick={props.handleAdd}>
-          <i className="fa fa-plus"></i>
-        </IconButton>
-        <IconButton style="info" icon="search" onClick={props.handleSearch} />
-        <IconButton
-          style="defalt"
-          icon="close"
-          onClick={props.handleClear}
-        ></IconButton>
-      </Grid>
-    </div>
-  );
-};
+        <Grid cols="12 3 2">
+          <IconButton style="primary" icon="plus" onClick={this.props.handleAdd} />
+          <IconButton style="info" icon="search" onClick={this.props.handleSearch} />
+          <IconButton style="default" icon="close" onClick={this.props.handleClear} />
+        </Grid>
+      </div>
+    );
+  }
+}
 
-const mapStateToProps = (state) => ({ description: state.todo.description });
+// Mantemos a ligação com o Redux, mas não sobrescreveremos a prop `description`
+// que vem do componente pai. Mapearemos o estado Redux para `reduxDescription`.
+const mapStateToProps = (state) => ({ reduxDescription: state.todo.description });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ changeDescription }, dispatch);
+  bindActionCreators({ changeDescription, search }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
