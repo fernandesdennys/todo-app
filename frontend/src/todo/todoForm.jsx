de3e5ad601311d71ba73fbcc0ef4f6d06abc4e79
Dennys Fernandes <dennys.projetos@outlook.com>
2025-11-18 16:17:51 -0300

    Integrate Redux for todo state management
    Added redux and react-redux dependencies. Implemented reducers, actions, and connected TodoForm and TodoList components to the Redux store for managing todo state. Updated index.jsx to provide the store to the app.

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
+import { changeDescription } from './todoActions';
 
-export default (props) => {
+const TodoForm = (props) => {
   const keyHandler = (e) => {
     if (e.key === 'Enter') {
-      e.shiftKey ? props.handleSearch() : props.handleAdd()
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
     } else if (e.key === 'Escape') {
-      props.handleClear()
+      props.handleClear();
     }
-  }
+  };
 
   return (
     <div role="form" className="todoForm">
@@ -18,21 +22,28 @@ export default (props) => {
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"
-          onChange={props.handleChange}
+          onChange={props.changeDescription}
           onKeyUp={keyHandler}
           value={props.description}
         />
       </Grid>
-  
+
       <Grid cols="12 3 2">
-        <IconButton style='primary' icon="plus" onClick={props.handleAdd}>
+        <IconButton style="primary" icon="plus" onClick={props.handleAdd}>
           <i className="fa fa-plus"></i>
         </IconButton>
-        <IconButton style="info" icon='search'
-        onClick={props.handleSearch}/>
-        <IconButton style='defalt' icon='close'
-        onClick={props.handleClear}></IconButton>
+        <IconButton style="info" icon="search" onClick={props.handleSearch} />
+        <IconButton
+          style="defalt"
+          icon="close"
+          onClick={props.handleClear}
+        ></IconButton>
       </Grid>
     </div>
   );
-}
+};
+
+const mapStateToProps = (state) => ({ description: state.todo.description });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ changeDescription }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
---
+++ frontend/src/todo/todoReducer.js
@@ -0,0 +1,25 @@
+const INITIAL_STATE = {
+  description: 'Ler livro',
+  list: [{
+    _id: 1,
+    description: 'Pagar fatura do cartão de crédito',
+    done: true
+  }, {
+    _id: 2,
+    description: 'Reunião com a equipe às 10h',
+    done: false
+  }, {
+    _id: 3,
+    description: 'Consultar médicos na terça-feira depois do almoço',
+    done: false
+  }]
+};
+
+export default (state = INITIAL_STATE, action) => {
+switch (action.type) {
+  case 'DESCRIPTION_CHANGED':
+    return { ...state, description: action.payload };
+  default:
+    return state;
+  }
+}
